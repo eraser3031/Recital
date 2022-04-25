c50460d17aa01261c9685a2a59cf62f69f074036
era_i_ser <eraser3031@gmail.com>
2022-04-25 20:16:11 +0900

    Initial Commit

---
+++ RecordTicket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecordTicket
+//
+//  Created by 김예훈 on 2022/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecordTicket/RecordTicketApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordTicketApp.swift
+//  RecordTicket
+//
+//  Created by 김예훈 on 2022/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RecordTicketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
