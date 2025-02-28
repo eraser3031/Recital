cce6c0c491232cba432b8e7e343cd4a7dbeda0e2
Yaehoon Kim <eraser3031@gmail.com>
2025-02-28 17:43:45 +0900

    ERA-131 사용하지 않는 코드 제거

--- RecordTicket/RecordTicketApp.swift
+++ RecordTicket/RecordTicketApp.swift
@@ -6,16 +6,12 @@
 //
 
 import SwiftUI
-import AVKit
 
 @main
 struct RecordTicketApp: App {
-//    let persistenceController = PersistenceController.shared
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
