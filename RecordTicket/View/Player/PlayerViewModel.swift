ad961a84c8970019a77cb514019de71063086467
Yaehoon Kim <eraser3031@gmail.com>
2025-02-28 17:39:54 +0900

    ERA-134 차후 데이터 흐름 로직 개편을 위한 ViewModel 추가

---
+++ RecordTicket/View/Player/PlayerViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerViewModel.swift
+//  RecordTicket
+//
+//  Created by 김예훈 on 2/28/25.
+//
+
+import Observation
+
+@MainActor
+@Observable final class PlayerViewModel {
+    // TODO: 로직 분리하기
+}
